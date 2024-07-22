81e7b4221c8def1950a624f7d25f43b158b02a95
BhargaviDharmavaram <bhargavidharmavaram2001@gmail.com>
2024-07-22 22:04:10 +0530

    added summary-report apis

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,8 @@ const configureDB = async () => {
     try{
         const db = await mongoose.connect('mongodb://127.0.0.1:27017/nkc')
         console.log('connecting to db')
-    }catch(e){
-        alert(e.message)
+    }catch(error){
+        console.log(error, 'error connecting to DB')
     }
 }
 module.exports = configureDB
