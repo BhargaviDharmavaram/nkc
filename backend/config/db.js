c3e0976c4571c6c01c720cd223c9a00334d9d54e
BhargaviDharmavaram <bhargavidharmavaram2001@gmail.com>
2024-08-06 11:42:13 +0530

    modified the db file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,31 @@
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const configureDB = async () => {
+//     try {
+//         const db = await mongoose.connect(process.env.MONGODB_URL, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+//         console.log('Connected to DB');
+//     } catch (error) {
+//         console.log('Error connecting to DB:', error);
+//     }
+// };
+
+// module.exports = configureDB;
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const configureDB = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGODB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const dbURI = process.env.MONGODB_URL;
+        if (!dbURI) {
+            throw new Error('MONGODB_URL environment variable is not set');
+        }
+
+        // Removed deprecated options
+        await mongoose.connect(dbURI);
         console.log('Connected to DB');
     } catch (error) {
         console.log('Error connecting to DB:', error);
